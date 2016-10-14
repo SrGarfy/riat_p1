8538f729baf167b1f26532d92e74ae16d491bd8f
Sr.Garfy <sr.garfy@gmail.com>
2016-10-14 12:18:37 +0400

    start

---
+++ riat_p1/riat_p1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace riat_p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
