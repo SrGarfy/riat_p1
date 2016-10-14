c3e50d5810794c30c529e8f0fbe6b3c543999ee5
Sr.Garfy <sr.garfy@gmail.com>
2016-10-14 12:47:02 +0400

    corefix

--- riat_p1/riat_p1/Comparer.cs
+++ riat_p1/riat_p1/Comparer.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace riat_p1
 {
-    class Comparer
+    class Comparer : IComparer<string>
     {
+        public int Compare(string a, string b)
+        {
+            return 0;
+        }
     }
 }
--- riat_p1/riat_p1/Program.cs
+++ riat_p1/riat_p1/Program.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;   
 
 namespace riat_p1
 {
+    delegate int myDelegate(string a, string b);
+
     class Program
     {
+        static int compare(string a, string b)
+        {
+            return 0;
+        }
+
+        static string[] interfaceSort(string[] strings, IComparer<string> comparer)
+        {
+            return strings;
+        }
+
+        static string[] funcSort(string[] strings, Func<string, string, int> comparer)
+        {
+            return strings;
+        }
+
+        static string[] delegateSort(string[] strings, myDelegate comparer)
+        {
+            return strings;
+        }
         static void Main(string[] args)
         {
+            var strings = new[] {"aaa", "bbbbb", "cccc", "dd"};
+            var interfaceSorted = interfaceSort(strings, new Comparer());
+            var funcSorted = funcSort(strings, compare);
+            var delegateSorted1 = delegateSort(strings, compare);
+            var delegateSorted2 = delegateSort(strings, (a,b) => (a == b ? 1 : 0));
         }
     }
 }
