22c8d1d2b17b67b5ad9bbba238bb57cc177677f8
Sr.Garfy <sr.garfy@gmail.com>
2016-10-14 14:00:10 +0400

    delegate

--- riat_p1/riat_p1/Program.cs
+++ riat_p1/riat_p1/Program.cs
@@ -39,17 +39,50 @@ namespace riat_p1
 
         static string[] delegateSort(string[] strings, myDelegate comparer)
         {
+            for (var i = 0; i < strings.Length; i++)
+            {
+                for (var j = i; j < strings.Length; j++)
+                {
+                    if (compare(strings[i], strings[j]))
+                    {
+                        var tmp = strings[i];
+                        strings[i] = strings[j];
+                        strings[j] = tmp;
+                    }
+                }
+            }
             return strings;
         }
         static void Main(string[] args)
         {
             var strings = new[] {"aaa", "bbbbb", "cccc", "dd"};
+            Console.WriteLine("Source array:");
+            foreach (var s in strings)
+            {
+                Console.WriteLine(s);
+            }
             var interfaceSorted = interfaceSort(strings, new Comparer());
             var funcSorted = funcSort(strings, compare);
             var delegateSorted1 = delegateSort(strings, compare);
-            var delegateSorted2 = delegateSort(strings, (a,b) => (a == b));
+            var delegateSorted2 = delegateSort(strings, (a, b) => {return a.Length > b.Length ? true : false;});
 
+            Console.WriteLine("Interface sorted:");
+            foreach (var s in interfaceSorted)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Func sorted:");
             foreach (var s in funcSorted)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Delegate (method) sorted:");
+            foreach (var s in delegateSorted1)
+            {
+                Console.WriteLine(s);
+            }
+            Console.WriteLine("Delegate (lambda) sorted:");
+            foreach (var s in delegateSorted2)
             {
                 Console.WriteLine(s);
             }
