f0ca4eb7c83c66de690957b99895b3a473d0d4ab
Sr.Garfy <sr.garfy@gmail.com>
2016-10-14 12:40:44 +0400

    core

---
+++ riat_p1/riat_p1/Comparer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace riat_p1
+{
+    class Comparer
+    {
+    }
+}
